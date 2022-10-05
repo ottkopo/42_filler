3e749d9ebfb37a5bbae7c216b12b7e190193fc50
Otto Koponen <58331418+ottkopo@users.noreply.github.com>
2022-10-05 11:31:23 +0300

    Update run_game.sh

--- tests/run_game.sh
+++ tests/run_game.sh
@@ -1,1 +1,1 @@
-../resources/filler_vm -p2 ../briffard.filler -p1 ../okoponen.filler -f ../resources/maps/map02
+../resources/filler_vm -p2 ../*opponent_player*.filler -p1 ../okoponen.filler -f ../resources/maps/*map*
